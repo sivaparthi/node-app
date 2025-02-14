ab707708b540bcc817defecd8bd77615b965408a
sivask-p <sivasundara@navriti.com>
2025-02-14 10:58:41 +0530

    feat: versioning

---
+++ .releaserc.js
@@ -0,0 +1,50 @@
+const branchFromRef = process.env.GITHUB_REF && process.env.GITHUB_REF.replace(/^refs\/heads\//, '');
+const currentBranch = process.env.SEMANTIC_RELEASE_BRANCH || branchFromRef || '';
+
+const plugins = [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator'
+];
+
+if (currentBranch === 'production') {
+  plugins.push(
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'node update-version.js ${nextRelease.version}'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    // This plugin creates a GitHub release.
+    [
+      '@semantic-release/github',
+      {
+        // You can configure additional options if needed.
+        // For example, if you want to attach assets, set them here:
+        // assets: 'build/**/*'
+      }
+    ]
+  );
+}
+
+module.exports = {
+  branches: [
+    'production',
+    { name: 'new_uat', prerelease: 'uat' },
+    { name: 'new_qa', prerelease: 'qa' },
+    { name: 'dev', prerelease: 'dev' }
+  ],
+  plugins
+};
